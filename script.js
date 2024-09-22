15f5ae36f8ac9a57a195a4f5a23e611c6d9e29ed
Crimeboi64 <129346856+Crimeboi64@users.noreply.github.com>
2024-09-22 15:17:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function One() {
+    document.getElementById("Alex1").style.marginLeft = "200px";
+  }
+function Truth() {
+    document.getElementById("maid").style.zIndex = "3"
+    document.getElementById("html").style.backgroundColor = "black"
+}
+
+function Two() {
+    document.getElementById("Title").innerHTML = "Alex Russano";
+  }
+
+function Three() {
+    document.getElementById("Alex2").style.opacity = "1"
+  }
+
+function Four() {
+    document.getElementById("Death").style.opacity = "1";
+  }
+
